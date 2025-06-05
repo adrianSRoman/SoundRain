dfcf2433aace7228cd54abd08ee3d2dd542f6c21
asroman <asroman@ucdavis.edu>
2025-06-04 22:03:01 -0700

    update padding for foa data

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 
 from trainer.base_trainer import BaseTrainer
-from util.utils import compute_STOI, compute_PESQ
+#from util.utils import compute_STOI, compute_PESQ
 plt.switch_backend('agg')
 
 
@@ -38,6 +38,7 @@ class Trainer(BaseTrainer):
             self.optimizer.step()
 
             loss_total += loss.item()
+            print("Loss ", loss_total)
 
         dl_len = len(self.train_data_loader)
         self.writer.add_scalar(f"Train/Loss", loss_total / dl_len, epoch)
@@ -60,12 +61,12 @@ class Trainer(BaseTrainer):
             name = name[0]
             padded_length = 0
 
-            mixture = mixture.to(self.device)  # [1, 1, T]
+            mixture = mixture.to(self.device)  # [1, 4, T]
 
             # The input of the model should be fixed length.
             if mixture.size(-1) % sample_length != 0:
                 padded_length = sample_length - (mixture.size(-1) % sample_length)
-                mixture = torch.cat([mixture, torch.zeros(1, 1, padded_length, device=self.device)], dim=-1)
+                mixture = torch.cat([mixture, torch.zeros(1, 4, padded_length, device=self.device)], dim=-1)
 
             assert mixture.size(-1) % sample_length == 0 and mixture.dim() == 3
             mixture_chunks = list(torch.split(mixture, sample_length, dim=-1))
@@ -84,61 +85,6 @@ class Trainer(BaseTrainer):
 
             assert len(mixture) == len(enhanced) == len(clean)
 
-            # Visualize audio
-            if i <= visualize_audio_limit:
-                self.writer.add_audio(f"Speech/{name}_Noisy", mixture, epoch, sample_rate=16000)
-                self.writer.add_audio(f"Speech/{name}_Enhanced", enhanced, epoch, sample_rate=16000)
-                self.writer.add_audio(f"Speech/{name}_Clean", clean, epoch, sample_rate=16000)
+        score = 0
 
-            # Visualize waveform
-            if i <= visualize_waveform_limit:
-                fig, ax = plt.subplots(3, 1)
-                for j, y in enumerate([mixture, enhanced, clean]):
-                    ax[j].set_title("mean: {:.3f}, std: {:.3f}, max: {:.3f}, min: {:.3f}".format(
-                        np.mean(y),
-                        np.std(y),
-                        np.max(y),
-                        np.min(y)
-                    ))
-                    librosa.display.waveplot(y, sr=16000, ax=ax[j])
-                plt.tight_layout()
-                self.writer.add_figure(f"Waveform/{name}", fig, epoch)
-
-            # Visualize spectrogram
-            noisy_mag, _ = librosa.magphase(librosa.stft(mixture, n_fft=320, hop_length=160, win_length=320))
-            enhanced_mag, _ = librosa.magphase(librosa.stft(enhanced, n_fft=320, hop_length=160, win_length=320))
-            clean_mag, _ = librosa.magphase(librosa.stft(clean, n_fft=320, hop_length=160, win_length=320))
-
-            if i <= visualize_spectrogram_limit:
-                fig, axes = plt.subplots(3, 1, figsize=(6, 6))
-                for k, mag in enumerate([
-                    noisy_mag,
-                    enhanced_mag,
-                    clean_mag,
-                ]):
-                    axes[k].set_title(f"mean: {np.mean(mag):.3f}, "
-                                      f"std: {np.std(mag):.3f}, "
-                                      f"max: {np.max(mag):.3f}, "
-                                      f"min: {np.min(mag):.3f}")
-                    librosa.display.specshow(librosa.amplitude_to_db(mag), cmap="magma", y_axis="linear", ax=axes[k], sr=16000)
-                plt.tight_layout()
-                self.writer.add_figure(f"Spectrogram/{name}", fig, epoch)
-
-            # Metric
-            stoi_c_n.append(compute_STOI(clean, mixture, sr=16000))
-            stoi_c_e.append(compute_STOI(clean, enhanced, sr=16000))
-            pesq_c_n.append(compute_PESQ(clean, mixture, sr=16000))
-            pesq_c_e.append(compute_PESQ(clean, enhanced, sr=16000))
-
-        get_metrics_ave = lambda metrics: np.sum(metrics) / len(metrics)
-        self.writer.add_scalars(f"Metric/STOI", {
-            "Clean and noisy": get_metrics_ave(stoi_c_n),
-            "Clean and enhanced": get_metrics_ave(stoi_c_e)
-        }, epoch)
-        self.writer.add_scalars(f"Metric/PESQ", {
-            "Clean and noisy": get_metrics_ave(pesq_c_n),
-            "Clean and enhanced": get_metrics_ave(pesq_c_e)
-        }, epoch)
-
-        score = (get_metrics_ave(stoi_c_e) + self._transform_pesq_range(get_metrics_ave(pesq_c_e))) / 2
         return score
